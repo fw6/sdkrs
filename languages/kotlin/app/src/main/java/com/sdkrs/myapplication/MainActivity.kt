6db3ad8a5dc2533a780e6e64a00b21094a394de1
feng.w <feng.w@trip.com>
2024-07-18 20:10:38 +0800

    feat: Add lazy loading for images in MainActivity

--- languages/kotlin/app/src/main/java/com/sdkrs/myapplication/MainActivity.kt
+++ languages/kotlin/app/src/main/java/com/sdkrs/myapplication/MainActivity.kt
@@ -1,32 +1,59 @@
 package com.sdkrs.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.fragment.app.FragmentActivity
+import com.sdkrs.core.DynamicImage
 import com.sdkrs.myapplication.ui.theme.MyApplicationTheme
+import com.sdkrs.sdk.getDynamicImageUrl
 
 class MainActivity : FragmentActivity() {
+    val TAG: String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             MyApplicationTheme {
-	@@ -99,342 +26,14 @@ class MainActivity : FragmentActivity() {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
                     Column(
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
+                        Row {
+                            Text(text = "Test my sdkrs sdk")
+                        }
+
+                        Divider()
+
                         Button(onClick = {
-                            // TODO
+                            Log.i(TAG, getDynamicImageUrl(DynamicImage(
+                                "https://youimg1.c-ctrip.com/target/fd/tg/g3/M07/01/AD/CggYGVXdMjCAYC76AE_wQZoMows816.jpg",
+                                240u,
+                                null,
+                                null,
+                                null,
+                                null,
+                                null,
+                                null,
+                                null,
+                                null,
+                            )))
                         }) {
-                            Text(text = "Test sdkrs")
+                            Text(text = "call dynamic image url")
                         }
                     }
                 }
